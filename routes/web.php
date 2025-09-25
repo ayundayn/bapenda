31d4e6e0d3064a0fd954f57f9e600adad677baff
Ayunda Yecantika Nurliyanti <yesayesi1303@gmail.com>
2025-09-25 15:32:17 +0700

    Login

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,31 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HasilTagihanController;
 use App\Http\Controllers\TagihanController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dashboard\Analytics;
 use App\Http\Controllers\form_elements\BasicInput;
 use App\Http\Controllers\form_elements\InputGroups;
 use App\Http\Controllers\tables\Basic as TablesBasic;
 
-// Main Page Route
-Route::get('/', [Analytics::class, 'index'])->name('dashboard-analytics');
+// Halaman dashboard (hanya untuk user login)
+Route::get('/', [Analytics::class, 'index'])
+    ->middleware(['auth'])
+    ->name('dashboard-analytics');
 
 // form elements
-Route::get('/forms/basic-inputs', [BasicInput::class, 'index'])->name('forms-basic-inputs');
-Route::get('/forms/input-groups', [InputGroups::class, 'index'])->name('forms-input-groups');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/forms/basic-inputs', [BasicInput::class, 'index'])->name('forms-basic-inputs');
+    Route::get('/forms/input-groups', [InputGroups::class, 'index'])->name('forms-input-groups');
 
-// tables
-Route::get('/tables/basic', [TablesBasic::class, 'index'])->name('tables-basic');
+    // tables
+    Route::get('/tables/basic', [TablesBasic::class, 'index'])->name('tables-basic');
 
-Route::get('/upload', [TagihanController::class, 'index'])->name('upload.form');
-Route::post('/proses', [TagihanController::class, 'proses'])->name('upload.proses');
-// Route::get('/download', [TagihanController::class, 'download'])->name('download.excel');
-Route::get('/hasil', [TagihanController::class, 'hasil'])->name('hasil.view');
-//Route::post('/proses', [TagihanController::class, 'proses'])->name('proses.excel');
-// Route::get('/download', [TagihanController::class, 'download'])->name('download.excel'); // ⬅ INI WAJIB ADA
-Route::get('/download-excel', [HasilTagihanController::class, 'downloadExcel'])->name('download.excel');
+    // upload & hasil
+    Route::get('/upload', [TagihanController::class, 'index'])->name('upload.form');
+    Route::post('/proses', [TagihanController::class, 'proses'])->name('upload.proses');
+    Route::get('/hasil', [TagihanController::class, 'hasil'])->name('hasil.view');
+    Route::get('/download-excel', [HasilTagihanController::class, 'downloadExcel'])->name('download.excel');
+});
+
+require __DIR__.'/auth.php';
